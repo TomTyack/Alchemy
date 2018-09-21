a17b214672bd9a0efb591ca7a723b38d6110646e
Thomas Tyack <ttyack@hotmail.com>
2018-09-21 10:26:11 +1000

    Renaming

---
+++ src/Foundation/RankingFoundry/code/Configuration/ConfigurationUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Web.Hosting;
+
+namespace Unicorn.Configuration
+{
+	internal static class ConfigurationUtility
+	{
+		/// <summary>
+		/// Applies the available formats for a Unicorn configuration path (i.e. root-relative, absolute) to a given path
+		/// </summary>
+		public static string ResolveConfigurationPath(string configPath)
+		{
+			if (configPath.StartsWith("~/"))
+			{
+				// +1 to Stack Overflow:
+				// http://stackoverflow.com/questions/4742257/how-to-use-server-mappath-when-httpcontext-current-is-nothing
+
+				// Support unit testing scenario where hosting environment is not initialized.
+				var hostingRoot = HostingEnvironment.IsHosted
+					? HostingEnvironment.MapPath("~/")
+					: AppDomain.CurrentDomain.BaseDirectory;
+
+				return Path.Combine(hostingRoot, configPath.Substring(2).Replace('/', '\\'));
+			}
+
+			return configPath;
+		}
+	}
+}
---
+++ src/Foundation/RankingFoundry/code/Configuration/MicroConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Configy.Containers;
+
+namespace Unicorn.Configuration
+{
+	public class MicroConfiguration : MicroContainer, IConfiguration
+	{
+		public MicroConfiguration(string name, string description, string extends, string[] dependencies, string[] ignoredDependencies) : base(name, extends)
+		{
+			Description = description;
+			Dependencies = dependencies ?? new string[0];
+			IgnoredImplicitDependencies = ignoredDependencies ?? new string[0];
+		}
+
+		public string Description { get; }
+		public string[] Dependencies { get; set; }
+		public string[] IgnoredImplicitDependencies { get; set; }
+	}
+}
